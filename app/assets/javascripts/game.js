70f8c96618898623ce94e4ffdddffca9a4e76f5d
Fex Doody <fexdoody@Fexs-MacBook-Pro.local>
2016-09-26 15:40:31 -0700

    animation and added example levels

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -302,7 +302,7 @@ function trackKeys(codes) {
     if (codes.hasOwnProperty(event.keyCode)){
       var down = event.type == "keydown";
       pressed[codes[event.keyCode]]= down;
-      event.precentDefault();
+      event.preventDefault();
     }
   }
   addEventListener("keydown", handler);
@@ -310,3 +310,47 @@ function trackKeys(codes) {
   return pressed;
 }
 
+function runAnimation(frameFunc) {
+  var lastTime = null;
+  function frame(time){
+    var stop = false;
+    if (lastTime != null) {
+      var timeStep = Math.min(time - lastTime, 100)/1000;
+      stop = frameFunc(timeStep) === false;
+    }
+    lastTime = time;
+    if (!stop)
+      requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+var arrows = trackKeys(arrowCodes);
+
+function runLevel(level,Display, andThen) {
+  var display = new Display(document.body, level);
+  runAnimation(function(step){
+    level.animate(step,arrows);
+    display.drawFrame(step);
+    if (level.isFinished()){
+      display.clear();
+      if (andThen)
+        andThen(level.status);
+      return false;
+    }
+  });
+}
+function runGame(plans,Display) {
+  function startLevel(n){
+    runLevel(new Level(plans[n]), Display, function(status){
+      if (status == "lost")
+        startLevel(n);
+      else if (n < plans.length -1)
+        startLevel(n+1);
+      else
+        console.log("You Win");
+    });
+  }
+  startLevel(0);
+}
+console.log("game should work");
